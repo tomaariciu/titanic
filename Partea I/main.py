9388321f2768ad31042d5339815d90a022cae462
Toma Ariciu <tomaariciu@gmail.com>
2024-05-29 14:16:53 +0300

    Added task 2 and updated gitignore
    Signed-off-by: Toma Ariciu <tomaariciu@gmail.com>

--- Partea I/main.py
+++ Partea I/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
 from task1 import task1
+from task2 import task2
 
 df = task1()
+task2(df)
---
+++ Partea I/task2.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_survivors(df):
+	total = len(df)
+	survived = df['Survived'].sum()
+	percentage = survived / total * 100
+	labels = ['Survived', 'Lost']
+	values = [percentage, 100 - percentage]
+	plt.pie(values, labels=labels)
+	plt.savefig("survivors.png")
+	plt.close()
+	pass
+
+def plot_classes(df):
+	total = len(df)
+	classes = df['Pclass'].value_counts()
+	labels = classes.index
+	values = classes.values
+	plt.pie(values, labels=labels)
+	plt.savefig("classes.png")
+	plt.close()
+	pass
+
+def plot_sexes(df):
+	sexes = df['Sex'].value_counts()
+	labels = sexes.index
+	values = sexes.values
+	plt.pie(values, labels=labels)
+	plt.savefig("sexes.png")
+	plt.close()
+	pass
+
+def task2(df):
+	plot_survivors(df)
+	plot_classes(df)
+	plot_sexes(df)
+	pass
