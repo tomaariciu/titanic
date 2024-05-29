49b0a4087b2d3be6feabea20355251242f92d806
Toma Ariciu <tomaariciu@gmail.com>
2024-05-30 00:30:06 +0300

    Added implementation for task 4
    Signed-off-by: Toma Ariciu <tomaariciu@gmail.com>

--- Partea I/main.py
+++ Partea I/main.py
@@ -1,7 +1,9 @@
 from task1 import task1
 from task2 import task2
 from task3 import task3
+from task4 import task4
 
 df = task1()
 task2(df)
 task3(df)
+task4(df)
---
+++ Partea I/task4.py
@@ -0,0 +1,22 @@
+def print_percentage(df):
+	for column in df.columns:
+		if (df[column].isna().sum() > 0):
+			empty = df[column].isna().sum()
+			total = len(df[column])
+			print(f"{column}:")
+			print(f"Total number is {empty}")
+			percentage = empty / total * 100
+			print(f"The percentage is {percentage}%")
+	pass
+
+def task4(df):
+	print("Total:")
+	print_percentage(df)
+	survived = df[df['Survived'] == 1]
+	not_survived = df[df['Survived'] == 0]
+	print("Survived:")
+	print_percentage(survived)
+	print("Not survived:")
+	print_percentage(not_survived)
+	pass
+
